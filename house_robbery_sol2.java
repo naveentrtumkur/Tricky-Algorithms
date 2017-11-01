c6cbf8f8f2bc694478fae6e9c06d4a7c35c51f98
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-11-01 09:21:12 -0400

    Alternate Approach-2
    Another Approach for DP solution to House Robbery question.

---
+++ house_robbery_sol2.java
@@ -0,0 +1,20 @@
+/*******
+Using an array by the name DP[num.length+1][2]
+DP[i][0] --- Not robbed
+DP[i][1] --- House robbed
+
+*****/
+
+import java.util.*;
+class Solution {
+    public int rob(int[] nums) {
+        
+        int dp[][] = new int[nums.length+1][2];
+        for(int i=1;i<=nums.length;i++)
+        {
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1] = nums[i-1] + dp[i-1][0]; 
+        }
+        return Math.max(dp[nums.length][0],dp[nums.length][1]);   
+    }
+}
