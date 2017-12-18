39659f8893d4e0fe95814d8c192892acc928d6e6
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-12-18 17:55:57 -0500

    Excel number and title.
    Convert to excel title and excel name.

---
+++ excel_sheet_title.java
@@ -0,0 +1,15 @@
+// Given en excel sheet title, convert it into appropriate excel name
+
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder result = new StringBuilder();
+
+        while(n>0){
+            n--;
+            result.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+
+        return result.toString();
+    }
+}
