6f05d64e5038945a193f82ee65d5fecdd20a6042
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-12-19 09:42:17 -0500

    Merge two sorted arrays.
    Written a program to merge two sorted arrays into the first sorted array. Assume the first array has enough space to hold all the elements.

---
+++ merge_two_sorted_arrays.java
@@ -0,0 +1,11 @@
+
+//Merge Two sorted array into the first sorted array.
+//HINT: The first sorted array would contain enough space to hold all teh elements.
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i=m-1, j=n-1, k=m+n-1;
+    while (i>-1 && j>-1)nums1[k--]= (nums1[i]>nums2[j]) ? nums1[i--] : nums2[j--];
+    while (j>-1)         nums1[k--]=nums2[j--];
+    }
+}
