08c578fed938fec782a882fcf1b91b29e6ff7a06
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-12-31 10:46:12 -0500

    Group anagrams.
    Group anagrams into a list.

---
+++ group_anagrams.java
@@ -0,0 +1,17 @@
+//Group anagrams using Java.
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+            //System.out.println("value==" +ans.get(key));
+        }
+        return new ArrayList(ans.values());
+    }
+}
