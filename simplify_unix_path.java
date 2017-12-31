b3748030d0a9de50482e582ce14294cfedaf73b4
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-12-31 11:02:19 -0500

    Simplify paths.
    Simplify paths using Java.

---
+++ simplify_unix_path.java
@@ -0,0 +1,17 @@
+//Simplify unix path using java.
+// I have used stack, and String.join to implement this problem.
+
+class Solution {
+    public String simplifyPath(String path) {
+    Stack<String> stack = new Stack<>();
+    String[] p = path.split("/");
+    for (int i = 0; i < p.length; i++) {
+        if (!stack.empty() && p[i].equals(".."))
+            stack.pop();
+        else if (!p[i].equals(".") && !p[i].equals("") && !p[i].equals(".."))
+            stack.push(p[i]);
+    }
+    List<String> list = new ArrayList(stack);
+    return "/"+String.join("/", list);
+}
+}
