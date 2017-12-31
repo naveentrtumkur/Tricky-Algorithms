cf5e3f3e156313f72229c4d884e0892d3f0c069e
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-12-31 11:06:52 -0500

    count and sort colours.

---
+++ sort_colours_2pass.java
@@ -0,0 +1,4 @@
+//Sort th ecolours using counts and two pass approach.
+
+Approach #1: A two pass approach
+I will count the number of reds,blues and greens and overwrite the array in second pass with these values.
