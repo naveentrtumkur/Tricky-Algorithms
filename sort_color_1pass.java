68d1dff5d8641e8b98b21f3b63bacef3a57f7b98
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-12-31 11:16:28 -0500

    sort the colors.
    Using a one pass approach.

---
+++ sort_color_1pass.java
@@ -0,0 +1,22 @@
+//Sort he colors red,blue and green suing one pass solution.
+
+class Solution {
+    public void sortColors(int[] nums) {
+    // 1-pass solution using end and start pointers.
+    int p1 = 0, p2 = nums.length - 1, i = 0;
+    while (i <= p2) {
+        if (nums[i] == 0) {
+            nums[i] = nums[p1];
+            nums[p1] = 0;
+            p1++;
+        }
+        if (nums[i] == 2) {
+            nums[i] = nums[p2];
+            nums[p2] = 2;
+            p2--;
+            i--;
+        }
+        i++;
+    }
+}
+}
