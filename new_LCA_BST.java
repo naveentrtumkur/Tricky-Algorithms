b0caed0230687927e0eae4066ad3ed4bbe8a1943
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-01-01 18:04:07 -0500

    New LCA
    new condition

---
+++ new_LCA_BST.java
@@ -0,0 +1,24 @@
+//New approach to checvking lowest common ancestor of a BST.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p == null || q == null)
+            return root;
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }else{
+            return root;
+        }
+    }
+}
