a305304c48d3dc92e099fc79272b013a79275cc3
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-02-17 15:54:24 -0500

    peak and valley approAch max profit stocks.
    Max profit stocks

---
+++ best_time_buystocks2_approach3.java
@@ -0,0 +1,23 @@
+// We need to sum the difference between all the peaks and valleys.
+// By this we will arrive at the maximum profit required.
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0 || prices == null)
+            return 0;
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
+    }
+}
