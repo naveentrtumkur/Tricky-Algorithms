37c64a7a4703e92fa2c69db09e5fe98798f8564c
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-02-18 21:15:19 -0500

    Move_zerosto_end.
    WAP to move the zeros to end of the list.

---
+++ moveZeros_toEnd.java
@@ -0,0 +1,18 @@
+// Write a function to move all the zeros to th eend.
+// Avoid using extra operations.
+
+class Solution {
+   public void moveZeroes(int[] nums) {
+    int leftMostZeroIndex = 0; // The index of the leftmost zero
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            if (i > leftMostZeroIndex) { // There are zero(s) on the left side of the current non-zero number, swap!
+                nums[leftMostZeroIndex] = nums[i];
+                nums[i] = 0;
+            }
+
+            leftMostZeroIndex++;
+        }
+    }
+}
+}
