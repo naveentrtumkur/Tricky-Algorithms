170933233648fc57dfef95268cd6f7e1a6dc375e
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-02-19 12:28:49 -0500

    Merge 2 sorted array and output the result.
    Merge two sorted arrays and output teh result.

---
+++ merge_2_sorted_arrays.java
@@ -0,0 +1,18 @@
+// Write a fucntion to merge two sorted arrays using Java.
+// Given that the first array would have enoiugh storage capacity to hold all the elements.
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1, j = n-1, k = m+n -1;
+        while(i>-1 && j>-1)   
+        {
+            nums1[k--] = (nums1[i] >nums2[j])?nums1[i--]:nums2[j--];
+            
+        }
+        while(j>-1)
+        {
+            nums1[k--] = nums2[j--];
+        }
+        
+    }
+}
