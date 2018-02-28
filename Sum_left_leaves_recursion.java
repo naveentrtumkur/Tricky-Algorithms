f8f9ad486f128c456decee7d897b6f0c1a8fc060
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-02-28 12:40:37 -0500

    Sum of left leaves of BST Using recursion.
    Sum of all left leaves of a BST

---
+++ Sum_left_leaves_recursion.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+        int sum =0;
+        if(root == null )
+            return 0;
+        if(root.left!=null && root.left.left == null && root.left.right== null)
+            sum = sum + root.left.val;
+         
+            return sum+sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+            //else
+             //  return sum+sumOfLeftLeaves(root.right);
+        
+    }
+}
