792b90ed80b0d7c85406956ff5a6e77e33f80610
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-03-01 21:30:48 -0500

    updation
    updation of code.

--- sorted_array_toBST.java
+++ sorted_array_toBST.java
@@ -17,3 +17,40 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
  -10  5
 
 ***/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        //Null check for nums array.
+        if(nums == null)
+            return null;
+        
+        // Construct a root node by using a helper function.
+        TreeNode root = helper(nums,0,nums.length-1);
+        return root;
+        /*int len = nums.length;
+        int low = 0, high = len-1,mid =0;*/
+        
+    }
+    public TreeNode helper(int[] nums, int low, int high)
+    {
+        if(low>high)
+            return null; // Condition to check so that we stop at this step.
+        
+        int mid = (low + high)/2;
+        TreeNode head = new TreeNode(nums[mid]) ;
+        //head.val = nums[mid];
+        head.left = helper(nums,low,mid-1);
+        head.right = helper(nums,mid+1,high);
+        //we need to return teh value of head.
+        return head;
+    }
+}
