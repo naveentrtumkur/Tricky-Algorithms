d881d12180783c62b8f217c179debd7e0459c0a2
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-03-22 16:15:07 -0400

    custom sort string, new approach.
    solving custom sort string.

--- custom_sort_string.java
+++ custom_sort_string.java
@@ -1,25 +1,42 @@
 //Leetcode customised string question to solve and arrange secopnd string similar to the foirst string.
 
-//attempt-1
-public class Solution {
+class Solution {
     public String customSortString(String S, String T) {
         
-        StringBuilder  list = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         Set<Character> set = new HashSet<>();
-        
-        for(int i=0; i < S.length(); i++)
-        {
-            list.append(S.charAt(i));
-            set.add(S.charAt(i));
-        }
+        int[] count = new int[26];
         
         for(int i=0;i<T.length();i++)
         {
-            if(!set.contains(T.charAt(i)))
-                list.append(T.charAt(i));
+            count[T.charAt(i) - 'a']++;
             
+            //set.add(T.charAt(i));
         }
         
-        return list.toString();//.toString();
+        //Parse the first string and construct the string buffer
+        for(int i=0;i<S.length();i++)
+        {
+            if(count[S.charAt(i)-'a'] > 0)
+            {
+                while(count[S.charAt(i)-'a']-- > 0)
+                    sb.append(S.charAt(i));
+                set.add(S.charAt(i));
+            }
+        }
+        
+        //Add the remaining T string characters to the SB.
+        for(int i=0;i<T.length();i++)
+        {
+            //System.out.println("char="+T.charAt(i));
+            if(!set.contains(T.charAt(i)))
+            {
+                //System.out.println("Inside if="+T.charAt(i));
+                sb.append(T.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+        
+        }
     }
-}
